953aeeb23edbc80a6e93e2bbf8db574fe2db467f
lengai <371522155@qq.com>
2018-06-09 02:13:46 +0800

    update func

--- func.py
+++ func.py
@@ -1,17 +1,22 @@
 import time, importlib
 from decimal import Decimal, ROUND_DOWN
 
-
-def Log(**kwargs):
-    print(kwargs)
+from services import transaction_log_serv
 
 
-def Sleep(time):
-    time.sleep(time)
+def Log(*args, **kwargs):
+    print(args, kwargs)
 
 
-def _C(func):
-    pass
+def Sleep(sleep_time):
+    time.sleep(sleep_time / 1000)
+
+
+def _C(func, *args, **kwargs):
+    while True:
+        result = func(*args, **kwargs)
+        if result:
+            return result
 
 
 def _N(num, precision):
@@ -19,10 +24,8 @@ def _N(num, precision):
     return num
 
 
-def getExchange(class_name, quote_currency, base_currency):
-    global exchange
+def getExchange(class_name, quote_currency, base_currency, access_key, secrect_key):
     module = importlib.import_module('lib.' + class_name)
-    # exchange = module.Exchange('e052bd66-424f0c16-ea398791-f3841', 'e1368ec2-ca459353-2b97a1b2-7c965', quote_currency,base_currency)
-    exchange = module.Exchange('7f80ef59-ba8d4dc0-51957f5a-40785', 'f7041765-c81c6671-cf19b32c-3f48d', quote_currency,
-                               base_currency)
+    exchange = module.Exchange(access_key, secrect_key, quote_currency, base_currency)
     return exchange
+
